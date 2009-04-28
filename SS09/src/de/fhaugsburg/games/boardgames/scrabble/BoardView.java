cf43cbfd234dec24d69608704e6a2e75d428108d
Marc Rochow <contact@gironimo.org>
2009-04-28 17:41:02 +0000

    now it looks like the meixner board

--- SS09/src/de/fhaugsburg/games/boardgames/scrabble/BoardView.java
+++ SS09/src/de/fhaugsburg/games/boardgames/scrabble/BoardView.java
@@ -7,34 +7,62 @@ public class BoardView  {
 	
 	public void render(ScrabbleBoard whatRender) {
 		
+		int labelRight= 'A';
 		int rows = whatRender.getHeight();
 		int columns = whatRender.getWidth();
+		System.out.print(" ");
 		
-		for (int s = rows-1; s >= 0; s--) {
+		for (int i = 1; i <= 15; i++) {
+			if (i < 10) {
+				System.out.print(" " + i + " ");
+			} else {
+				System.out.print(" " + i);
+			}
+		}
+		
+		System.out.println();
+		
+		for (int s = rows-1, i = 65; s >= 0; s--, i++) {
+			System.out.print((char)i);
 			for (int z = 0; z < columns; z++) {
-				if (whatRender.getSquare(z, s).getPiece() == null) {
-					if (whatRender.getSquare(s, z).multi == SquareMultiplier.Dummy) {
+				if(whatRender.getSquare(z, s).getPiece() == null) {
+					if(whatRender.getSquare(s, z).multi == SquareMultiplier.Dummy) {
 						System.out.print(" . ");
 					}
-					if (whatRender.getSquare(s, z).multi== SquareMultiplier.LetterMultiplier_2) {
+					if(whatRender.getSquare(s, z).multi== SquareMultiplier.LetterMultiplier_2) {
 						System.out.print(".. ");
 					}
-					if (whatRender.getSquare(s, z).multi == SquareMultiplier.LetterMultiplier_3) {
+					if(whatRender.getSquare(s, z).multi == SquareMultiplier.LetterMultiplier_3) {
 						System.out.print("...");
 					}
-					if (whatRender.getSquare(s, z).multi == SquareMultiplier.WordMultiplier_3) {
+					if(whatRender.getSquare(s, z).multi == SquareMultiplier.WordMultiplier_3) {
 						System.out.print("___");
 					}
-					if (whatRender.getSquare(s, z).multi == SquareMultiplier.WordMultiplier_2) {
+					if(whatRender.getSquare(s, z).multi == SquareMultiplier.WordMultiplier_2) {
 						System.out.print("__ ");
 					}
-				}
-				else {
+				} else {
 					System.out.print(" " + ((LetterPiece)whatRender.getSquare(z, s).getPiece()).name() + " ");
+				}
+				if (z >= 14) {
+					System.out.print((char) labelRight);
+					labelRight++;
 				}
 			}
 			System.out.println();
 		}
+		
+		System.out.print(" ");
+		
+		for (int i = 1; i <= 15; i++) {
+			if(i < 10) {
+				System.out.print(" " + i + " ");
+			} else {
+				System.out.print(" " + i);
+			}
+		}
+		
+		System.out.println();
 	}
 	
 }
